3e324c1b328ec52bab7d962e84c138f2365fc8fc
stgeary <scottmgeary17@gmail.com>
2024-04-12 14:00:10 -0500

    DAWGGGGGGG

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,13 +5,14 @@
     <nav id="navbar">
       <h1>Tail Wag & Whisker Wear</h1>
       <a href="#">Home</a>
-
+      
       <a href="#">Dogs</a>
-
+      
       <a href="#">Cats</a>
-
+      
       <a href="#">Advice</a>
     </nav>
+    <img src= "./logo.png" width= "400px" height= "200px" />
   </header>
 </template>
 
