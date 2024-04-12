3bb2f85d57bc1c2b57d8f4e3a62bfa96dc43a769
zachlengel <156560805+zachlengel@users.noreply.github.com>
2024-04-12 14:39:20 -0400

    added header style and links

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,9 +3,14 @@
 <template>
   <header>
     <nav id="navbar">
+      <h1>Tail Wag & Whisker Wear</h1>
       <a href="#">Home</a>
-      |
-      <a href="#">Link</a>
+
+      <a href="#">Dogs</a>
+
+      <a href="#">Cats</a>
+
+      <a href="#">Advice</a>
     </nav>
   </header>
 </template>
