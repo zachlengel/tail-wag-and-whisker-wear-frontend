0501130f4038729d313f4a8e9ef598679bfbff3b
zachlengel <156560805+zachlengel@users.noreply.github.com>
2024-04-12 13:32:22 -0400

    added placeholder data and variables

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -8,8 +8,20 @@ export default {
   data: function () {
     return {
       outfits: [
-        { id: 1, name: "First", url: "https://via.placeholder.com/150", width: 150, height: 150 },
-        { id: 2, name: "Second", url: "https://via.placeholder.com/300", width: 300, height: 300 },
+        {
+          id: 1,
+          name: "Blonde Wig",
+          url: "https://www.inspiremore.com/wp-content/uploads/2021/06/Dogs-wearing-wigs-1.jpg",
+          price: "$19.99",
+          description: "Make you dog feel like a star in this very fashionable wig.",
+        },
+        {
+          id: 2,
+          name: "Real Hair Wig",
+          url: "https://m.media-amazon.com/images/I/61Oh2US5ZuL.jpg",
+          price: "$99.99",
+          description: "A wig made of real human hair to make your pet feel normal.",
+        },
       ],
     };
   },
