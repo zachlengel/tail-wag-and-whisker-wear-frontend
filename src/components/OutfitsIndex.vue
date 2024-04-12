a0ca10c3fbd5cf26cf4ad73e72f28cddf1b1c730
zachlengel <156560805+zachlengel@users.noreply.github.com>
2024-04-12 12:58:37 -0400

    added index

---
+++ src/components/OutfitsIndex.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  props: {
+    outfits: Array,
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1>All outfits</h1>
+    <div v-for="outfit in outfits" v-bind:key="outfit.id">
+      <h2>{{ outfit.name }}</h2>
+      <img v-bind:src="outfit.url" />
+      <p>Width: {{ outfit.width }}</p>
+      <p>Height: {{ outfit.height }}</p>
+    </div>
+  </div>
+</template>
+
+<style></style>
